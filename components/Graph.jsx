719fe05949b02bfbfd4eb4b7fcb0ee9ade0bdc68
Emir-Danisik <emir.danisik@hotmail.com>
2024-02-25 06:57:09 -0600

    Initial commit

---
+++ components/Graph.jsx
@@ -0,0 +1,26 @@
+"use client"
+import { Bar, BarChart, ResponsiveContainer, XAxis, YAxis } from "recharts";
+
+export const Overview = ({ data }) => {
+  return (
+    <ResponsiveContainer width="80%" height={350}>
+      <BarChart data={data}>
+        <XAxis
+          dataKey="name"
+          stroke="#888888"
+          fontSize={16}
+          tickLine={false}
+          axisLine={false}
+        />
+        <YAxis
+          stroke="#888888"
+          fontSize={16}
+          tickLine={false}
+          axisLine={false}
+          tickFormatter={(value) => `${value}`}
+        />
+        <Bar dataKey="total" fill="#3498db" radius={[4, 4, 0, 0]} />
+      </BarChart>
+    </ResponsiveContainer>
+  )
+};
